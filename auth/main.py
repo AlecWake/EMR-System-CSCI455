b29e152558701830235003f4bff47ad8c3050f7f
Logan-Evans-1 <evanslogan23@hotmail.com>
2026-04-25 13:06:19 -0500

    Update main.py

--- auth/main.py
+++ auth/main.py
@@ -1,1 +1,11 @@
+from enum import Enum
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
 
+#Never used this stuff, if you find an easy fix in something, please leave a comment with a little explanation so i can figure out this stuff better
+api = FastAPI()
+
+@api.get("/test/")
+async def test():
+  return{"test1a" : "test2b"}
